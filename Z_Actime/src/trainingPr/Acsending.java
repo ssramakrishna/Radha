137e1d5be5ad6173f471658ef17eae8a154ea0d8
ssrag <ssrag@SSRamakrishna>
2023-02-08 17:04:27 +0530

    update

--- Z_Actime/src/trainingPr/Acsending.java
+++ Z_Actime/src/trainingPr/Acsending.java
@@ -5,9 +5,12 @@ public class Acsending {
 		int[] a = { 7, 4, 2, 41, 1, 9, 0 };
 		int temp = 0;
 
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < a.length; j++) {
-				if (a[i] < a[j]) {
+		for (int i = 0; i < a.length; i++) 
+		{
+			for (int j = 0; j < a.length; j++) 
+			{
+				if (a[i] < a[j]) 
+				{
 					temp = a[i];
 					a[i] = a[j];
 					a[j] = temp;
